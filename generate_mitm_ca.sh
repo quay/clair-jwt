af7b76b6946003193926bf756be3b90a15787c29
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2017-02-22 19:32:42 -0500

    dockerfile: convert to Alpine base image

--- generate_mitm_ca.sh
+++ generate_mitm_ca.sh
@@ -1,10 +1,9 @@
-#! /bin/bash
+#! /bin/sh
 set -e
 
 mkdir /certificates
 echo '{"CN":"CA","key":{"algo":"rsa","size":2048}}' | cfssl gencert -initca - | cfssljson -bare mitm
 cp mitm-key.pem /certificates/mitm.key
 cp mitm.pem /certificates/mitm.crt
-cp mitm.pem /usr/local/share/ca-certificates/mitm.crt
-
-update-ca-certificates
+cp mitm.pem /etc/ssl/certs/mitm.crt
+update-ca-certificates --fresh
