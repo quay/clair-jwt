f2421a222eb1539df3f8fe4167823d26cba4bf5c
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2018-07-10 16:55:20 -0400

    add a /certs directory for mounting custom certs

--- generate_mitm_ca.sh
+++ generate_mitm_ca.sh
@@ -1,9 +1,15 @@
 #! /bin/sh
 set -e
 
-mkdir /certificates
+# Generate a MITM certificate and key
 echo '{"CN":"CA","key":{"algo":"rsa","size":2048}}' | cfssl gencert -initca - | cfssljson -bare mitm
+mkdir -p /certificates
 cp mitm-key.pem /certificates/mitm.key
 cp mitm.pem /certificates/mitm.crt
 cp mitm.pem /usr/local/share/ca-certificates/mitm.crt
+
+# This directory is for any custom certificates users want to mount
+mkdir -p /certs
+cp /certs/* /usr/local/share/ca-certificates
+
 update-ca-certificates --fresh
